f3aef08e130800f6e9e592250e79ce6007baada0
BenTStark <B.Ruf@hotmail.de>
2018-10-22 21:45:50 +0200

    Implementing Redux

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
-ReactDOM.render(<App />, document.getElementById('root'))
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, browserHistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import { ReduxAsyncConnect } from "redux-connect";
+
+//import rootReducer from './reducers/reducer';
+import App from "./app";
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunkMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+const history = syncHistoryWithStore(browserHistory, store);
+
+// Find Object in index.html for starting point.
+const destination = document.getElementById("root");
+// tbd: checken ob das passt und wie das ReduxAsyncConnect genau funktioniert
+const component = (
+  <Router
+    render={props => (
+      <ReduxAsyncConnect {...props} filter={item => !item.deferred} />
+    )}
+    history={history}
+  >
+    <App />
+  </Router>
+);
+
+ReactDOM.render(
+  <Provider store={store} key="provider">
+    {component}
+  </Provider>
+);
